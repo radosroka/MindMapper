56db02784d071c59a0e35fde1ec4de379981e8bb
Radovan Sroka <rsroka@redhat.com>
2016-12-12 19:41:44 +0100

    Arrows really works

--- arrow.js
+++ arrow.js
@@ -6,38 +6,60 @@
 
  	var x_size = from.x - to.x;
  	var y_size = from.y - to.y;
- 	var angle = Math.atan2(to.y, to.x) * 180/3.14;
+ 	var angle = Math.atan2(y_size, x_size) * 180/3.14;
  	var length = Math.sqrt(Math.pow(x_size, 2) + Math.pow(y_size, 2));
-
- 	console.log(angle);
- 	console.log(x_size);
- 	console.log(y_size);
 
  	ctx.lineWidth = lineWidth * 0.7;
  	ctx.moveTo(from.x, from.y);
  	ctx.lineTo(to.x, to.y);
-
- 	if (x_size > 0 && y_size > 0){
- 		ctx.lineTo(to.x, to.y + length/10);
- 		ctx.lineTo(to.x + length/10, to.y);
- 		ctx.lineTo(to.x, to.y);
- 	}
- 	else if (x_size <= 0 && y_size <= 0){
- 		ctx.lineTo(to.x, to.y - length/10);
- 		ctx.lineTo(to.x - length/10, to.y);
- 		ctx.lineTo(to.x, to.y);
- 	}
- 	else if (x_size > 0 && y_size <= 0){
- 		ctx.lineTo(to.x, to.y - length/10);
- 		ctx.lineTo(to.x + length/10, to.y);
- 		ctx.lineTo(to.x, to.y);
- 	}
- 	else if (x_size <= 0 && y_size > 0){
- 		ctx.lineTo(to.x, to.y + length/10);
- 		ctx.lineTo(to.x - length/10, to.y);
- 		ctx.lineTo(to.x, to.y);
- 	}
-
  	ctx.fill();
  	ctx.stroke();
+
+ 	var len = ctx.lineWidth * 5;
+
+ 	var top = 90;
+ 	var low = 0;
+ 	var range = 2*len;
+ 
+
+ 	if (x_size > 0 && y_size > 0){
+
+ 		x = range / top * angle;
+ 		x = x - len;
+ 		ctx.lineTo(to.x + len, to.y + x);
+ 		ctx.lineTo(to.x, to.y);
+ 		ctx.lineTo(to.x - x, to.y + len);
+ 	}
+ 	else if (x_size <= 0 && y_size <= 0){
+
+ 		angle2 = 90 - ((angle * -1) - 90);
+ 		x = range / top * angle2;
+ 		x = x - len;
+ 		ctx.lineTo(to.x + x, to.y - len);
+ 		ctx.lineTo(to.x, to.y);
+ 		ctx.lineTo(to.x - len, to.y - x);
+ 	}
+ 	else if (x_size > 0 && y_size <= 0){
+
+ 		angle2 = 90 - (angle * -1);
+ 		x = range / top * angle2;
+ 		x = x - len;
+ 		ctx.lineTo(to.x + x, to.y - len);
+ 		ctx.lineTo(to.x, to.y);
+ 		ctx.lineTo(to.x + len, to.y + x);
+ 	}
+ 	else if (x_size <= 0 && y_size > 0){
+
+ 		angle2 = (angle) - 90;
+ 		x = range / top * angle2;
+ 		x = x - len;
+ 		ctx.lineTo(to.x - x, to.y + len);
+ 		ctx.lineTo(to.x, to.y);
+ 		ctx.lineTo(to.x - len, to.y - x);
+ 	}
+
+ 	ctx.stroke();
+ 	ctx.shadowBlur = 15;
+ 	ctx.shadowOffsetX = 4;
+	ctx.shadowOffsetY = 4;
  }
